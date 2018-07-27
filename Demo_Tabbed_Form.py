d1b14520797349f590d1a60f99383883ee057774
MikeTheWatchGuy <mike_barnett@hotmail.com>
2018-07-27 15:58:59 -0400

    Removed autosize text setting
    Required for 2.6.

--- Demo_Tabbed_Form.py
+++ Demo_Tabbed_Form.py
@@ -76,7 +76,7 @@ def eBaySuperSearcherGUI():
             layout_tab_2.append([sg.Text('Typical US Search String')])
             layout_tab_2.append([sg.InputText(size=(100,1), default_text='gruen -sara -quarz -quartz -embassy -bob -robert -elephants -adidas -LED ')])
             layout_tab_2.append([sg.Text('_' * 100, size=(75, 1))])
-            layout_tab_2.append([sg.Submit(button_color=('red', 'yellow'),auto_size_text=True), sg.Cancel(button_color=('white', 'blue'), auto_size_text=True)])
+            layout_tab_2.append([sg.Submit(button_color=('red', 'yellow')), sg.Cancel(button_color=('white', 'blue'))])
 
             results = sg.ShowTabbedForm('eBay Super Searcher', (form,layout_tab_1,'Where To Save'), (form2, layout_tab_2, 'Categories & Search String'))
 
