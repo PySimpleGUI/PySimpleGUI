ce352ea0bfd90716ba72be8a5de83d81b4bd89d9
MikeTheWatchGuy <mike_barnett@hotmail.com>
2018-09-05 19:57:32 -0400

    New feature - option to remove title bar from forms! Targets for Calendar buttons, hiding tabbed forms until completely built before showing,

--- Demo_Calendar.py
+++ Demo_Calendar.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
 
 layout = [[sg.T('Calendar Test')],
-          [sg.CalendarButton('Choose Date', key='date')],
+          [sg.In('', size=(20,1))],
+          [sg.CalendarButton('Choose Date', target=(1,0), key='date')],
           [sg.Ok(key=1)]]
 
-form = sg.FlexForm('Calendar')
+form = sg.FlexForm('Calendar', no_titlebar=True)
 b,v = form.LayoutAndRead(layout)
 sg.Popup(v['date'])
