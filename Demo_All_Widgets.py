c8b394c5be5f5c747cf0396393b9dbb95af81a60
MikeTheWatchGuy <mike_barnett@hotmail.com>
2018-09-19 10:43:30 -0400

    Demo the Text Element relief setting

--- Demo_All_Widgets.py
+++ Demo_All_Widgets.py
@@ -11,13 +11,13 @@ column1 = [[sg.Text('Column 1', background_color='#F7F3EC', justification='cente
            [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3')]]
 
 layout = [
-    [sg.Text('All graphic widgets in one form!', size=(30, 1), font=("Helvetica", 25))],
+    [sg.Text('All graphic widgets in one form!', size=(30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
     [sg.Text('Here is some text.... and a place to enter text')],
     [sg.InputText('This is my text')],
 
     [sg.Frame(layout=[
     [sg.Checkbox('Checkbox'),  sg.Checkbox('My second checkbox!', default=True)],
-    [sg.Radio('My first Radio!     ', "RADIO1", default=True), sg.Radio('My second Radio!', "RADIO1")]], title='Options',title_color='red', relief=sg.RELIEF_RAISED)],
+    [sg.Radio('My first Radio!     ', "RADIO1", default=True), sg.Radio('My second Radio!', "RADIO1")]], title='Options',title_color='red', relief=sg.RELIEF_SUNKEN)],
     [sg.Multiline(default_text='This is the default Text should you decide not to type anything', size=(35, 3)),
      sg.Multiline(default_text='A second multi-line', size=(35, 3))],
     [sg.InputCombo(('Combobox 1', 'Combobox 2'), size=(20, 1)),
