afbaa5ef971d50b761bdf19788c46da567cb2971
MikeTheWatchGuy <mike_barnett@hotmail.com>
2018-09-24 12:17:45 -0400

    Renamed MultiTab to TabGroup

--- Demo_Tabs.py
+++ Demo_Tabs.py
@@ -11,11 +11,12 @@ tab6_layout = [[sg.T('This is inside tab 6')],
                [sg.T('How about a second row of stuff in tab 6?')]]
 
 layout = [[sg.T('My Window!')],
-    [sg.MultiTab([[sg.Tab('Tab 1', tab1_layout), sg.Tab('Tab 2', tab2_layout)]]), sg.MultiTab([[sg.Tab('Tab 3', tab3_layout), sg.Tab('Tab 4', tab4_layout)]])],
+    [sg.TabGroup([[sg.Tab('Tab 1', tab1_layout), sg.Tab('Tab 2', tab2_layout)]]), sg.TabGroup([[sg.Tab('Tab 3', tab3_layout), sg.Tab('Tab 4', tab4_layout)]])],
     [sg.T('Text in the middle of the mess')],
-    [sg.MultiTab([[sg.Tab('Tab 5', tab5_layout), sg.Tab('Tab 6', tab6_layout)]])],
+    [sg.TabGroup([[sg.Tab('Tab 5', tab5_layout), sg.Tab('Tab 6', tab6_layout)]])],
           [sg.RButton('Read')],
           ]
+
 window = sg.Window('My window with tabs').Layout(layout)
 
 while True:
