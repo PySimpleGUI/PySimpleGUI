517c3fadac24a718f64cbdf0b8345447fd8bcda7
MikeTheWatchGuy <mike_barnett@hotmail.com>
2018-10-29 16:29:23 -0400

    Test of new update method for Tree Element

--- Demo_Tree_Element.py
+++ Demo_Tree_Element.py
@@ -16,9 +16,13 @@ treedata.Insert("_A_", '_A2_', 'A2', [None, None])
 treedata.Insert("_A1_", '_A3_', 'A30', ['getting deep'])
 treedata.Insert("_C_", '_C2_', 'C2', ['nothing', 'at', 'all'])
 
+for i in range(100):
+    treedata.Insert('_C_', i, i, [])
+
 layout = [[ sg.Text('Tree Test') ],
-          [ sg.Tree(data=treedata, headings=['col1', 'col2', 'col3'], auto_size_columns=True, num_rows=10, col0_width=10, key='_tree_', show_expanded=True)],
-          [ sg.Button('Read')]]
+          [ sg.Tree(data=treedata, headings=['col1', 'col2', 'col3'],change_submits=True, auto_size_columns=True, num_rows=10, col0_width=10, key='_TREE_', show_expanded=True),
+            ],
+          [ sg.Button('Read'), sg.Button('Update')]]
 
 window = sg.Window('Tree Element Test').Layout(layout)
 
@@ -28,4 +32,14 @@ while True:     # Event Loop
     event, values = window.Read()
     if event is None:
         break
-    print(event, values)
+    if event == 'Update':
+        treedata = sg.TreeData()
+        treedata.Insert("", '_A_', 'A', [1, 2, 3])
+        treedata.Insert("", '_B_', 'B', [4, 5, 6])
+        treedata.Insert("_A_", '_A1_', 'A1', ['can', 'be', 'anything'])
+        treedata.Insert("", '_C_', 'C', [])
+        treedata.Insert("_C_", '_C1_', 'C1', ['or'])
+        treedata.Insert("_A_", '_A2_', 'A2', [None, None])
+        window.FindElement('_TREE_').Update(treedata)
+    elif event == 'Read':
+        print(event, values)
