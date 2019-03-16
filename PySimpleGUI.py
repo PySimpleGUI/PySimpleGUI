a74b603a86dbce74ab5d765babb05cf4a965633e
MikeTheWatchGuy <mike_barnett@hotmail.com>
2019-03-16 17:13:34 -0400

    Enabled a default title in PopupGetFolder if one isn't specified

--- PySimpleGUI.py
+++ PySimpleGUI.py
@@ -7410,7 +7410,7 @@ def PopupGetFolder(message, title=None, default_path='', no_window=False, size=(
               [InputText(default_text=default_path, size=size), FolderBrowse(initial_folder=initial_folder)],
               [CloseButton('Ok', size=(5, 1), bind_return_key=True), CloseButton('Cancel', size=(5, 1))]]
 
-    window = Window(title=title, icon=icon, auto_size_text=True, button_color=button_color,
+    window = Window(title=title or message, icon=icon, auto_size_text=True, button_color=button_color,
                     background_color=background_color,
                     font=font, no_titlebar=no_titlebar, grab_anywhere=grab_anywhere, keep_on_top=keep_on_top,
                     location=location)
