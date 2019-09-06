ad394dc6a8beb3b3638bd5d513b0ce2b6ad4c699
MikeTheWatchGuy <mike_barnett@hotmail.com>
2019-09-06 11:09:01 -0400

    Official 4.4.1 & 2.4.1 releases

--- PySimpleGUI27.py
+++ PySimpleGUI27.py
@@ -12,7 +12,7 @@ from builtins import str
 from builtins import object
 from future import standard_library
 standard_library.install_aliases()
-version = __version__ = "2.4.1 Unreleased"
+version = __version__ = "2.4.1 Released"
 
 
 #  888888ba           .d88888b  oo                     dP           .88888.  dP     dP dP
