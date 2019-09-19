3b24d8921e264fe553a1ed4d8d31a3c15d85b0e0
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2019-09-19 13:32:25 -0400

    Update of ALL Matplotlib demos so they run on 3.1. Brought up to date too.

--- DemoPrograms/Demo_Matplotlib.py
+++ DemoPrograms/Demo_Matplotlib.py
@@ -84,7 +84,7 @@ def draw_figure(canvas, figure, loc=(0, 0)):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-
+    return figure_canvas_agg
 #------------------------------- Beginning of GUI CODE -------------------------------
 
 # define the window layout
@@ -96,6 +96,6 @@ layout = [[sg.Text('Plot test', font='Any 18')],
 window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI', layout, finalize=True)
 
 # add the plot to the window
-fig_photo = draw_figure(window['canvas'].TKCanvas, fig)
+fig_canvas_agg = draw_figure(window['canvas'].TKCanvas, fig)
 
 event, values = window.read()
--- DemoPrograms/Demo_Matplotlib_Animated.py
+++ DemoPrograms/Demo_Matplotlib_Animated.py
@@ -1,64 +1,55 @@
 #!/usr/bin/env python
-import sys
-if sys.version_info[0] >= 3:
-    import PySimpleGUI as sg
-else:
-    import PySimpleGUI27 as sg
+import PySimpleGUI as sg
 
 from random import randint
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, FigureCanvasAgg
 from matplotlib.figure import Figure
-import matplotlib.backends.tkagg as tkagg
-import tkinter as tk
 
+def draw_figure(canvas, figure, loc=(0, 0)):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
 
 def main():
-    fig = Figure()
 
+    NUM_DATAPOINTS = 10000
+    # define the form layout
+    layout = [[sg.Text('Animated Matplotlib', size=(40, 1), justification='center', font='Helvetica 20')],
+              [sg.Canvas(size=(640, 480), key='-CANVAS-')],
+              [sg.Text('Progress through the data')],
+              [sg.Slider(range=(0, NUM_DATAPOINTS), size=(60, 10), orientation='h', key='-SLIDER-')],
+              [sg.Text('Number of data points to display on screen')],
+               [sg.Slider(range=(10, 500), default_value=40, size=(40, 10), orientation='h', key='-SLIDER-DATAPOINTS-')],
+              [sg.Button('Exit', size=(10, 1), pad=((280, 0), 3), font='Helvetica 14')]]
+
+    # create the form and show it without the plot
+    window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI', layout, finalize=True)
+
+    canvas_elem = window.FindElement('-CANVAS-')
+    slider_elem = window.FindElement('-SLIDER-')
+    canvas = canvas_elem.TKCanvas
+
+    # draw the initial plot in the window
+    fig = Figure()
     ax = fig.add_subplot(111)
     ax.set_xlabel("X axis")
     ax.set_ylabel("Y axis")
     ax.grid()
+    fig_agg = draw_figure(canvas, fig)
+    # make a bunch of random data points
+    dpts = [randint(0, 10) for x in range(NUM_DATAPOINTS)]
 
-    # define the form layout
-    layout = [[sg.Text('Animated Matplotlib', size=(40, 1), justification='center', font='Helvetica 20')],
-              [sg.Canvas(size=(640, 480), key='canvas')],
-              [sg.Slider(range=(0, 10000), size=(60, 10), orientation='h', key='slider')],
-              [sg.ReadButton('Exit', size=(10, 2), pad=((280, 0), 3), font='Helvetica 14')]]
-
-    # create the form and show it without the plot
-    window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI').Layout(layout).Finalize()
-
-    canvas_elem = window.FindElement('canvas')
-    slider_elem = window.FindElement('slider')
-    graph = FigureCanvasTkAgg(fig, master=canvas_elem.TKCanvas)
-    canvas = canvas_elem.TKCanvas
-
-    dpts = [randint(0, 10) for x in range(10000)]
     for i in range(len(dpts)):
         event, values = window.Read(timeout=10)
         if event in ('Exit', None):
             exit(69)
-
-        slider_elem.Update(i)
-        ax.cla()
-        ax.grid()
-        DATA_POINTS_PER_SCREEN = 40
-        ax.plot(range(DATA_POINTS_PER_SCREEN), dpts[i:i+DATA_POINTS_PER_SCREEN],  color='purple')
-        graph.draw()
-        figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
-        figure_w, figure_h = int(figure_w), int(figure_h)
-        photo = tk.PhotoImage(master=canvas, width=figure_w, height=figure_h)
-
-        canvas.create_image(640/2, 480/2, image=photo)
-
-        figure_canvas_agg = FigureCanvasAgg(fig)
-        figure_canvas_agg.draw()
-
-        # Unfortunately, there's no accessor for the pointer to the native renderer
-        tkagg.blit(photo, figure_canvas_agg.get_renderer()._renderer, colormode=2)
-
-        # time.sleep(.1)
+        slider_elem.Update(i)       # slider shows "progress" through the data points
+        ax.cla()                    # clear the subplot
+        ax.grid()                   # draw the grid
+        data_points = int(values['-SLIDER-DATAPOINTS-']) # draw this many data points (on next line)
+        ax.plot(range(data_points), dpts[i:i+data_points],  color='purple')
+        fig_agg.draw()
 
 
 if __name__ == '__main__':
