7f1584d97ec89c2260676d785bf7c14fe7a8a523
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2019-10-07 16:37:01 -0400

    Refreshed Menu Demo

--- DemoPrograms/Demo_Menus.py
+++ DemoPrograms/Demo_Menus.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
-import sys
-if sys.version_info[0] >= 3:
-    import PySimpleGUI as sg
-else:
-    import PySimpleGUI27 as sg
+import PySimpleGUI as sg
 """
     Demonstration of MENUS!
     How do menus work?  Like buttons is how.
     Check out the variable menu_def for a hint on how to 
     define menus
 """
-def SecondForm():
+def second_window():
 
     layout = [[sg.Text('The second form is small \nHere to show that opening a window using a window works')],
               [sg.OK()]]
 
-    window = sg.Window('Second Form').Layout(layout)
-    b, v = window.Read()
+    window = sg.Window('Second Form', layout)
+    event, values = window.read()
+    window.close()
+
+def test_menus():
 
 
-def TestMenus():
-
-
-    sg.ChangeLookAndFeel('LightGreen')
-    sg.SetOptions(element_padding=(0, 0))
+    sg.change_look_and_feel('LightGreen')
+    sg.set_options(element_padding=(0, 0))
 
     # ------ Menu Definition ------ #
     menu_def = [['&File', ['&Open', '&Save', '&Properties', 'E&xit' ]],
@@ -33,32 +29,32 @@ def TestMenus():
 
     # ------ GUI Defintion ------ #
     layout = [
-            [sg.Menu(menu_def, tearoff=False, pad=(20,1))],
+              [sg.Menu(menu_def, tearoff=False, pad=(20,1))],
               [sg.Output(size=(60,20))],
-            [sg.Text('Status Bar', relief=sg.RELIEF_SUNKEN, size=(55,1),  pad=(0,3),key='_status_')]
               ]
 
     window = sg.Window("Windows-like program",
+                       layout,
                        default_element_size=(12, 1),
                        auto_size_text=False,
                        auto_size_buttons=False,
-                       default_button_element_size=(12, 1)).Layout(layout)
+                       default_button_element_size=(12, 1))
 
     # ------ Loop & Process button menu choices ------ #
     while True:
-        event, values = window.Read()
+        event, values = window.read()
         if event is None or event == 'Exit':
             return
         print('Event = ', event)
         # ------ Process menu choices ------ #
         if event == 'About...':
-            window.Disappear()
-            sg.Popup('About this program','Version 1.0', 'PySimpleGUI rocks...', grab_anywhere=True)
-            window.Reappear()
+            window.disappear()
+            sg.popup('About this program','Version 1.0', 'PySimpleGUI rocks...', grab_anywhere=True)
+            window.reappear()
         elif event == 'Open':
-            filename = sg.PopupGetFile('file to open', no_window=True)
+            filename = sg.popup_get_file('file to open', no_window=True)
             print(filename)
         elif event == 'Properties':
-            SecondForm()
+            second_window()
 
-TestMenus()
+test_menus()
