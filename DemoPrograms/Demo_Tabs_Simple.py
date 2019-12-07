fd553e67323f25f24b7f1f3c0487c05fc6553c4a
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2019-12-06 22:37:06 -0500

    Demonstrates new tab capabilities - colors, visiblility

--- DemoPrograms/Demo_Tabs_Simple.py
+++ DemoPrograms/Demo_Tabs_Simple.py
@@ -3,26 +3,38 @@ import PySimpleGUI as sg
 
 # Simple example of TabGroup element
 
+sg.change_look_and_feel('Dark Brown 1')
+
 tab1_layout = [[sg.Text('Tab 1')],
                [sg.Text('Put your layout in here')],
-               [sg.Text('Input something'), sg.Input(key='-IN0-')]]
+               [sg.Text('Input something'), sg.Input(size=(12,1), key='-IN0-')]]
 
-tab2_layout = [[sg.Text('Tab2')]]
-
-
-layout = [[sg.TabGroup([[
-                sg.Tab('Tab 1', tab1_layout),
-                sg.Tab('Tab 2', tab2_layout)]], key='-TABGROUP-')],
-          [sg.Button('Read')]]
-
-window = sg.Window('My window with tabs', layout,
-                   default_element_size=(12, 1))
+tab2_layout = [[sg.Text('Tab 2')]]
+tab3_layout = [[sg.Text('Tab 3')]]
 
 
+tab_group_layout = [[sg.Tab('Tab 1', tab1_layout, key='-TAB1-'),
+                     sg.Tab('Tab 2', tab2_layout, visible=False, key='-TAB2-'),
+                     sg.Tab('Tab 3', tab3_layout, key='-TAB3-')]]
+
+layout = [[sg.TabGroup(tab_group_layout,
+                       selected_title_color='blue',
+                       selected_background_color='red',
+                       tab_background_color='green',
+                       enable_events=True,
+                       key='-TABGROUP-')],
+          [sg.Text('Make tab number'), sg.Input(key='-IN-', size=(3,1)), sg.Button('Invisible'), sg.Button('Visible')]]
+
+window = sg.Window('My window with tabs', layout)
+
+tab_keys = ('-TAB1-','-TAB2-','-TAB3-')
 while True:
-    event, values = window.read()
-    sg.popup_non_blocking('button = %s' % event, 'Values dictionary', values)
-    if event is None:           # always,  always give a way out!
+    event, values = window.read()               # type: str, dict
+    print(event, values)
+    if event is None:
         break
-        
+    if event == 'Invisible':
+        window[tab_keys[int(values['-IN-'])-1]].update(visible=False)
+    if event == 'Visible':
+        window[tab_keys[int(values['-IN-'])-1]].update(visible=True)
 window.close()
