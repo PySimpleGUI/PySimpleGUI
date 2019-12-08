68a0c79d1a0d32e2bab798bdccee8bdf57fef7b9
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2019-12-08 12:50:16 -0500

    Better camera control for droid

--- DemoPrograms/PyDroid3/Demo_OpenCV_4_Line_Program.py
+++ DemoPrograms/PyDroid3/Demo_OpenCV_4_Line_Program.py
@@ -1,5 +1,6 @@
 import tkinter
 import cv2, PySimpleGUI as sg
-window, cap = sg.Window('Demo Application - OpenCV Integration', [[sg.Image(filename='', key='image')], ], location=(0, 0)), cv2.VideoCapture(0)
+USE_CAMERA = 0      # change to 1 for front facing camera
+window, cap = sg.Window('Demo Application - OpenCV Integration', [[sg.Image(filename='', key='image')], ], location=(0, 0), grab_anywhere=True), cv2.VideoCapture(USE_CAMERA)
 while window(timeout=20)[0] is not None:
     window['image'](data=cv2.imencode('.png', cap.read()[1])[1].tobytes())
