64e3ba11d5753287b7f7f4683eeb1e2a17df0dd9
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2020-10-18 12:25:02 -0400

    Switched __version__ back to old definition

--- PySimpleGUI.py
+++ PySimpleGUI.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-version = "4.30.0.4 Unreleased \nAdded ability to set icon for popup_get_file when icon is set as parameter, changed __version__  to be same as 'ver' (the shortened version number), added Window.set_cursor, changed install to use version instead of __version__"
+version = "4.30.0.5 Unreleased \nAdded ability to set icon for popup_get_file when icon is set as parameter, changed __version__  to be same as 'ver' (the shortened version number), added Window.set_cursor, changed install to use version instead of __version__, changed back __version__ to be the long-form of the version number so that installs from GitHub will work again."
 
 # The shortened version of version
 try:
@@ -7,7 +7,7 @@ try:
 except:
     ver = ''
 
-__version__ = ver
+__version__ = version
 
 port = 'PySimpleGUI'
 
