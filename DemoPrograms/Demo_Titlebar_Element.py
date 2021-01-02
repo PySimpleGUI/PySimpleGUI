b54c6dd8ec0fa979ff6afa59dfb4a559aa0efc53
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2021-01-02 13:21:08 -0500

    Added code and comments to show all 3 ways to create a custom titlebar

--- DemoPrograms/Demo_Titlebar_Element.py
+++ DemoPrograms/Demo_Titlebar_Element.py
@@ -2,28 +2,32 @@ import PySimpleGUI as sg
 
 """
     The Custom Titlebar Demo
-    Two ways of getting a custom titlebar:
+    3 ways of getting a custom titlebar:
     1. set_options - will create a titlebar that every window will have based on theme
     2. Titlebar element - Adds custom titlebar to your window
+    3. use_custom_titlebar parameter - Add to your Window object
 """
 
-sg.set_options(use_custom_titlebar=True)
+# sg.set_options(use_custom_titlebar=True)
+# sg.set_options(titlebar_background_color='red', titlebar_text_color='white', titlebar_font='courier 12', )
+
+
 
 def main():
-
     layout = [
-              # [sg.Titlebar('My Custom Titlebar', background_color='light blue', text_color='red', k='-T-')],
-              [sg.Text('My Window')],
-              [sg.Input(k='-IN1-')],
-              [sg.Input(k='-IN2-')],
-              [sg.Input(k='-IN3-')],
-              [sg.Button('Clear'), sg.Button('Popup'), sg.Button('Exit')]]
+        # [sg.Titlebar('My Custom Titlebar', background_color='light blue', text_color='red', k='-T-')],
+        [sg.Text('My Window')],
+        [sg.Input(k='-IN1-')],
+        [sg.Input(k='-IN2-')],
+        [sg.Input(k='-IN3-')],
+        [sg.Button('Clear'), sg.Button('Popup'), sg.Button('Exit')]]
 
     # Use the same title so that when the window minimizes, the title will be the same as the custom titlebar title
-    window = sg.Window('My Custom Titlebar', layout)
+    # window = sg.Window('My Custom Titlebar', layout)
+    window = sg.Window('My Custom Titlebar', layout, use_custom_titlebar=True)
 
     while True:
-        event, values = window()
+        event, values = window.read()
         print(event, values)
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
@@ -33,5 +37,6 @@ def main():
             sg.popup('This is a popup')
 
     window.close()
+
 
 main()
