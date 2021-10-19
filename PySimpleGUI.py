9c8c6a7047bf3e4c01356354c8b8d25f191e94de
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2021-10-19 11:26:57 -0400

    Release 4.51.2

--- PySimpleGUI.py
+++ PySimpleGUI.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 
-version = __version__ = "4.51.0.2 Unreleased"
+
+version = __version__ = "4.51.2 Released 19-Oct-2021"
 
 _change_log = """
 
-    Changelog since 4.51.0 release to PyPI on 18-Oct-2021
+    Changelog since 4.51.2 release to PyPI on 19-Oct-2021
 
-    4.51.0.1
-        New Right-Click Menu constant - MENU_RIGHT_CLICK_EDITME_VER_LOC_EXIT
-            Adds "File Location" to the standard list.
-            The code I've been using for the file location choice is:
-                if event == 'File Location':
-                    sg.popup_scrolled('This Python file is:', __file__)
-    4.51.0.2    
-        Fix for one_line_progress_meter with no_button=True when using a HORIZONTAL layout
+
     """
-
 __version__ = version.split()[0]  # For PEP 396 and PEP 345
 
 # The shortened version of version
