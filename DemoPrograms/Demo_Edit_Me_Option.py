f3270e00ecd4e3a21c6add112bdbb99848b32025
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2023-02-07 07:17:07 -0500

    And updated version of the "Edit Me" right menu design pattern

--- DemoPrograms/Demo_Edit_Me_Option.py
+++ DemoPrograms/Demo_Edit_Me_Option.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 
 """
-    Demo "Edit Me"
+    Demo "Edit Me" (and Version)
     
     More and more of these Demos are getting an "Edit me" option added.
     
@@ -12,26 +12,26 @@ import PySimpleGUI as sg
     You can add this capability to your program by adding a right click menu to your window and calling the
     editor that you set up in the global PySimpleGUI options.
     
-    You need to do 2 things to make this work:
-    1. Add a right click menu - requires you to add 1 parameter to your Window creation
-    2. Add 1 if statement to your event loop.
-
+    A constant MENU_RIGHT_CLICK_EDITME_VER_EXIT, when set at the right click menu shows a "Version" and "Edit Me" meny item.
+    
     You will need to have first set up your editor by using the menu in sg.main()
     
-    Copyright 2021 PySimpleGUI.org
+    Copyright 2021, 2022, 2023 PySimpleGUI.org
 """
 
 
 layout = [[sg.Text('Edit this program by right clicking and choosing "Edit me"')],
           [sg.Button('Exit')]]
 
-window = sg.Window('Edit Me Right Click Menu Demo', layout, right_click_menu=[[''], ['Edit Me', 'Exit',]])
+window = sg.Window('Edit Me Right Click Menu Demo', layout, right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_VER_EXIT)
 
 while True:  # Event Loop
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     if event == 'Edit Me':
-        sg.execute_editor(__file__)
+            sg.execute_editor(__file__)
+    elif event == 'Version':
+            sg.popup_scrolled(__file__, sg.get_versions(), location=window.current_location(), keep_on_top=True, non_blocking=True)
 
 window.close()
