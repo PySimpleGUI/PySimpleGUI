7ec3b16695fe3a04e7457fce2d9cada9f23a5c72
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2024-03-31 14:22:33 -0400

    Removed custom titlebar as it just made things messy, complicated.  Added second demo program just for JPG

--- DemoPrograms/Demo_Net_API_Download_Image.py
+++ DemoPrograms/Demo_Net_API_Download_Image.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 import PySimpleGUI as sg
 
-# Only need these imports if using JPGs
-from PIL import Image
-from io import BytesIO
-import base64
-
 
 """
-    Demo of Net API - Download Images using net_download_file_binary
+    Demo of Net API - Download a PNG Image using net_download_file_binary & Display in a Window
 
-    This Demo Program shows how to download a binary file (an image) and display in a window.
-    The PNG image display is quite easy.  Displaying a JPG requires converting using PIL.
+    This Demo Program shows how to download a binary file (an image) and display it in a window.
+
     
     Copyright 2018-2023 PySimpleSoft, Inc. and/or its licensors. All rights reserved.
     
@@ -25,19 +20,5 @@ sg.theme('black')
 
 # First the easy case.... display a PNG image
 
-sg.Window('BIG News', [[sg.Image(sg.net_download_file_binary(r'https://pysimplegui.net/images/big_news_emoji.png'))]],
-          use_custom_titlebar=True, titlebar_background_color='black', titlebar_text_color='white').read(close=True)
+sg.Window('PNG Download', [[sg.Image(sg.net_download_file_binary(r'https://pysimplegui.net/images/tests/powered-by-pysimplegui-5.png'))]]).read(close=True)
 
-
-# Now display a JPG image. It requires converting the image to PNG before using with PySimpleGUI
-jpg_data = sg.net_download_file_binary(r'https://pysimplegui.net/images/tests/powered-by-pysimplegui-5.jpg')
-
-jpg_image = Image.open(BytesIO(jpg_data))
-
-with BytesIO() as bio:
-    jpg_image.save(bio, format='PNG')
-    contents = bio.getvalue()
-    encoded = base64.b64encode(contents)
-
-sg.Window('Powered By', [[sg.Image(data=encoded)]],
-          use_custom_titlebar=True, titlebar_background_color='black', titlebar_text_color='white').read(close=True)
---
+++ DemoPrograms/Demo_Net_API_Download_Image_jpg.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import PySimpleGUI as sg
+
+# Only need these imports if using JPGs
+from PIL import Image
+from io import BytesIO
+
+
+"""
+    Demo of Net API - Download JPG Image using net_download_file_binary and display in a window
+    
+    The download is the easiest part.  It's the displaying of a JPG that's tricky.
+
+    
+    Copyright 2018-2023 PySimpleSoft, Inc. and/or its licensors. All rights reserved.
+    
+    Redistribution, modification, or any other use of PySimpleGUI or any portion thereof is subject to the terms of the PySimpleGUI License Agreement available at https://eula.pysimplegui.com.
+    
+    You may not redistribute, modify or otherwise use PySimpleGUI or its contents except pursuant to the PySimpleGUI License Agreement.
+"""
+
+
+# Download the binary file (into a variable, not a local file)
+jpg_data = sg.net_download_file_binary(r'https://pysimplegui.net/images/tests/powered-by-pysimplegui-5.jpg')
+
+# Now display a JPG image. It requires converting the image to PNG before using with PySimpleGUI
+jpg_image = Image.open(BytesIO(jpg_data))
+
+with BytesIO() as bio:
+    jpg_image.save(bio, format='PNG')
+    data = bio.getvalue()
+
+sg.Window('Download JPG', [[sg.Image(data=data)]]).read(close=True)
