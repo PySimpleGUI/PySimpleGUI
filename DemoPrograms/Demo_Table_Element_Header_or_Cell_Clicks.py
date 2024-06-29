231720943fc9ab5c49d7d1d6932dd4875a8c649f
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2024-06-29 16:04:50 -0400

    Added support for the new Table Element parameter enable_cell_editing

--- DemoPrograms/Demo_Table_Element_Header_or_Cell_Clicks.py
+++ DemoPrograms/Demo_Table_Element_Header_or_Cell_Clicks.py
@@ -5,29 +5,32 @@ import string
 import operator
 
 """
-    Table Element Demo With Sorting
+    Table Element Demo With Sorting and Cell Editing
 
     The data for the table is assumed to have HEADERS across the first row.
     This is often the case for CSV files or spreadsheets
 
-    In release 4.48.0 a new enable_click_events parameter was added to the Table Element
-    This enables you to click on Column Headers and individual cells as well as the standard Row selection
-
     This demo shows how you can use these click events to sort your table by columns
+    Additionally, in release 5.0.6.1 individual cell editing was added to the Table element using the parameter enable_cell_editing
 
-    Copyright 2022-2023 PySimpleSoft, Inc. and/or its licensors. All rights reserved.
-    
+    Copyright 2022-2024 PySimpleSoft, Inc. and/or its licensors. All rights reserved.
+
     Redistribution, modification, or any other use of PySimpleGUI or any portion thereof is subject to the terms of the PySimpleGUI License Agreement available at https://eula.pysimplegui.com.
-    
+
     You may not redistribute, modify or otherwise use PySimpleGUI or its contents except pursuant to the PySimpleGUI License Agreement.
 """
 
 sg.theme('Light green 6')
+
+
 # ------ Some functions to help generate data for the table ------
 def word():
     return ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+
+
 def number(max_val=1000):
     return random.randint(0, max_val)
+
 
 def make_table(num_rows, num_cols):
     data = [[j for j in range(num_cols)] for i in range(num_rows)]
@@ -36,10 +39,12 @@ def make_table(num_rows, num_cols):
         data[i] = [i, word(), *[number() for i in range(num_cols - 2)]]
     return data
 
+
 # ------ Make the Table Data ------
 data = make_table(num_rows=15, num_cols=6)
 # headings = [str(data[0][x])+'     ..' for x in range(len(data[0]))]
-headings = [f'Col {col}' for col in range(1,len(data[0])+1)]
+headings = [f'Col {col}' for col in range(1, len(data[0]) + 1)]
+
 
 def sort_table(table, cols):
     """ sort a table by multiple columns
@@ -55,6 +60,7 @@ def sort_table(table, cols):
             sg.popup_error('Error in sort_table', 'Exception in sort_table', e)
     return table
 
+
 # ------ Window Layout ------
 layout = [[sg.Table(values=data[1:][:], headings=headings + ['Extra'], max_col_width=25,
                     auto_size_columns=True,
@@ -68,7 +74,8 @@ layout = [[sg.Table(values=data[1:][:], headings=headings + ['Extra'], max_col_w
                     enable_events=True,
                     expand_x=True,
                     expand_y=True,
-                    enable_click_events=True,           # Comment out to not enable header and other clicks
+                    enable_click_events=True,  # Comment out to not enable header and other clicks
+                    enable_cell_editing=True,
                     tooltip='This is a table')],
           [sg.Button('Read'), sg.Button('Double'), sg.Button('Change Colors')],
           [sg.Text('Cell clicked:'), sg.T(k='-CLICKED-')],
@@ -81,8 +88,6 @@ window = sg.Window('The Table Element', layout,
                    # ttk_theme='clam',
                    resizable=True, right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_VER_EXIT, finalize=True)
 
-# Add the ability to double-click a cell
-window["-TABLE-"].bind('<Double-Button-1>' , "+-double click-")
 
 # ------ Event Loop ------
 while True:
@@ -94,22 +99,19 @@ while True:
         sg.execute_editor(__file__)
     elif event == 'Version':
         sg.popup_scrolled(__file__, sg.get_versions(), location=window.current_location(), keep_on_top=True, non_blocking=True)
+    if event == 'Read':
+        [print(row) for row in window['-TABLE-'].values]
     if event == 'Double':
         for i in range(1, len(data)):
             data.append(data[i])
         window['-TABLE-'].update(values=data[1:][:])
     elif event == 'Change Colors':
         window['-TABLE-'].update(row_colors=((8, 'white', 'red'), (9, 'green')))
-    elif event == '-TABLE-+-double click-':
-        print('Last cell clicked was', window['-TABLE-'].get_last_clicked_position())
-    if isinstance(event, tuple):
-        # TABLE CLICKED Event has value in format ('-TABLE=', '+CLICKED+', (row,col))
-        # You can also call Table.get_last_clicked_position to get the cell clicked
-        if event[0] == '-TABLE-':
-            if event[2][0] == -1 and event[2][1] != -1:           # Header was clicked and wasn't the "row" column
-                col_num_clicked = event[2][1]
-                new_table = sort_table(data[1:][:],(col_num_clicked, 0))
-                window['-TABLE-'].update(new_table)
-                data = [data[0]] + new_table
-            window['-CLICKED-'].update(f'{event[2][0]},{event[2][1]}')
+    if event[0] == '-TABLE-':
+        if event[2][0] == -1 and event[2][1] != -1:  # Header was clicked and wasn't the "row" column
+            col_num_clicked = event[2][1]
+            new_table = sort_table(data[1:][:], (col_num_clicked, 0))
+            window['-TABLE-'].update(new_table)
+            data = [data[0]] + new_table
+        window['-CLICKED-'].update(f'{event[2][0]},{event[2][1]}')
 window.close()
