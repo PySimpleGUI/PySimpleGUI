979d4082ec39dd009d4902b0be8b0a55e74c331e
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2024-07-02 08:43:37 -0400

    Updated Table Element Demo Program to include new Editing features

--- DemoPrograms/Demo_Table_Element_Header_or_Cell_Clicks.py
+++ DemoPrograms/Demo_Table_Element_Header_or_Cell_Clicks.py
@@ -6,12 +6,12 @@ import operator
 
 """
     Table Element Demo With Sorting and Cell Editing
-
+    NOTE: release 5.0.6.5 needed in order to use the Cell Editing features.  Comment out the parameters that contain cell_edit to remove from demo
+    
     The data for the table is assumed to have HEADERS across the first row.
     This is often the case for CSV files or spreadsheets
 
     This demo shows how you can use these click events to sort your table by columns
-    Additionally, in release 5.0.6.1 individual cell editing was added to the Table element using the parameter enable_cell_editing
 
     Copyright 2022-2024 PySimpleSoft, Inc. and/or its licensors. All rights reserved.
 
@@ -75,7 +75,9 @@ layout = [[sg.Table(values=data[1:][:], headings=headings + ['Extra'], max_col_w
                     expand_x=True,
                     expand_y=True,
                     enable_click_events=True,  # Comment out to not enable header and other clicks
-                    enable_cell_editing=True,
+                    enable_cell_editing=True,  # Comment out to if your PSG version does not support cell edint
+                    cell_edit_colors='white on blue', # Comment out to if your PSG version does not support cell edint
+                    cell_edit_select_colors='yellow on red', # Comment out to if your PSG version does not support cell edint
                     tooltip='This is a table')],
           [sg.Button('Read'), sg.Button('Double'), sg.Button('Change Colors')],
           [sg.Text('Cell clicked:'), sg.T(k='-CLICKED-')],
@@ -84,15 +86,12 @@ layout = [[sg.Table(values=data[1:][:], headings=headings + ['Extra'], max_col_w
           [sg.Text('Change Colors = Changes the colors of rows 8 and 9'), sg.Sizegrip()]]
 
 # ------ Create Window ------
-window = sg.Window('The Table Element', layout,
-                   # ttk_theme='clam',
-                   resizable=True, right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_VER_EXIT, finalize=True)
+window = sg.Window('The Table Element', layout, resizable=True, right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_VER_EXIT, finalize=True, print_event_values=True)
 
 
 # ------ Event Loop ------
 while True:
     event, values = window.read()
-    print(event, values)
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     if event == 'Edit Me':
@@ -103,11 +102,13 @@ while True:
         [print(row) for row in window['-TABLE-'].values]
     if event == 'Double':
         for i in range(1, len(data)):
-            data.append(data[i])
+            data.append(data[i].copy())
         window['-TABLE-'].update(values=data[1:][:])
     elif event == 'Change Colors':
         window['-TABLE-'].update(row_colors=((8, 'white', 'red'), (9, 'green')))
-    if event[0] == '-TABLE-':
+
+    # See if was a table clicked or edited event by checking event[0] for table's key
+    if event[0] == '-TABLE-':   # TABLE CELL Event has value in format ('-TABLE=', '+type of event+', (row,col))
         if event[2][0] == -1 and event[2][1] != -1:  # Header was clicked and wasn't the "row" column
             col_num_clicked = event[2][1]
             new_table = sort_table(data[1:][:], (col_num_clicked, 0))
