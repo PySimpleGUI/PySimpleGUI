edc1cb913f562768ec76705d68c22844179fb50f
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2026-04-14 12:25:13 -0400

    Fix missing comma

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
                     "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
                     "Intended Audience :: Developers",
                     "Operating System :: OS Independent",
-                    "Framework :: PySimpleGUI"
+                    "Framework :: PySimpleGUI",
                     "Programming Language :: Python :: 3",
                     "Programming Language :: Python :: 3.6",
                     "Programming Language :: Python :: 3.7",
