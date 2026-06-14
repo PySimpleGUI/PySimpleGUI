4be43bd777b83dc0fc4a909607b115cd50134ff0
PySimpleGUI <PySimpleGUI@PySimpleGUI.com>
2026-06-14 14:02:20 -0400

    Set default border color for Frame element to be the same as Text elem color

--- PySimpleGUI/PySimpleGUI.py
+++ PySimpleGUI/PySimpleGUI.py
@@ -54,7 +54,7 @@
 
 """
 
-version = "6.1.9"
+version = "6.1.10"
 
 
 
@@ -90,6 +90,7 @@ Changelog since last major release
 6.1.7        12-Jun-2026 Added typing ElementType for better type support when looking up elements using window[key]
 6.1.8        13-Jun-2026 Removed the ElementType changes.  Code completion breaks when used with key lookups
 6.1.9        14-Jun-2026 Enhancement - ability to set the Frame border color. Added border_color and border_width_no_relief parms to the Frame element
+6.1.10       14-Jun-2026 Set Frame border color to default to the text element color if no color is set
 """
 
 
@@ -6962,7 +6963,7 @@ class Frame(Element):
         :type p:                      (int, int) or ((int, int),(int,int)) or (int,(int,int)) or  ((int, int),int) | int
         :param border_width:          width of border around element in pixels (used with the frame's relief)
         :type border_width:           (int)
-        :param border_color:          Custom color of the border around frame.  Set by system if using a relief. Use border_width_no_relief with border_color to get a custom frame color
+        :param border_color:          Custom color of the border around frame.  Set by system if using a relief. Use border_width_no_relief with border_color to get a custom frame color. defaults to text elem color
         :type border_color:           (str)
         :param border_width_no_relief: width of border around element in pixels. This is used without a relief. border_width should be 0 if this parm is non-zero
         :type border_width_no_relief: (int)
@@ -7003,7 +7004,7 @@ class Frame(Element):
         self.Relief = relief
         self.TitleLocation = title_location
         self.BorderWidth = border_width
-        self.BorderColor = border_color
+        self.BorderColor = border_color if border_color is not None else theme_text_color()
         self.BorderWidthNoRelief = border_width_no_relief
         self.BackgroundColor = background_color if background_color is not None else DEFAULT_BACKGROUND_COLOR
         self.RightClickMenu = right_click_menu
