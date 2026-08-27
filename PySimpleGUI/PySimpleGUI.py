a5a7c4883dd366901a368e5e44b5d01509e7fe33
PySimpleGUI <46163555+PySimpleGUI@users.noreply.github.com>
2026-08-27 06:56:58 -0400

    Fix for the fix made in 6.3.11 when creating windows.  Needed to add back the alpha method of hiding the window when the window has no titlebar. The                                 withdraw method of hiding the window causes the titlebar to appear so can't use it for no titlebar windows

--- PySimpleGUI/PySimpleGUI.py
+++ PySimpleGUI/PySimpleGUI.py
@@ -54,7 +54,7 @@
 
 """
 
-version = "6.3.12"
+version = "6.3.13"
 
 
 
@@ -87,7 +87,9 @@ Changelog since last major release
                             every window is created. Rather than using Alpha to hide a window while it's being created, this version withdraws the window and uses a call I didn't
                             know about to get the size of the window before it's fully created. master.winfo_reqwidth() master.winfo_reqheight(). Alpha was needed before so
                             the window dimensions could be gathered to determine where to locate the window so it is centered on the screen.
-6.3.12      27-Aug-2026 Another bite of the fix-auto-save-location bug fix apple                            
+6.3.12      27-Aug-2026 Another bite of the fix-auto-save-location bug fix apple          
+6.3.13      27-Aug-2026 Fix for the fix made in 6.3.11 when creating windows.  Needed to add back the alpha method of hiding the window when the window has no titlebar. The
+                                withdraw method of hiding the window causes the titlebar to appear so can't use it for no titlebar windows                  
 """
  
 
@@ -18343,15 +18345,16 @@ def StartupTK(window):
 
     # If location is None, then there's no need to hide the window.  Let it build where it is going to end up being.
     if DEFAULT_HIDE_WINDOW_WHEN_CREATING is True and window.Location is not None:
-        root.withdraw()
-    #     try:
-    #         if not running_mac() or \
-    #                 (running_mac() and not window.NoTitleBar) or \
-    #                 (running_mac() and window.NoTitleBar and not _mac_should_apply_notitlebar_patch()):
-    #
-    #             root.attributes('-alpha', 0)  # hide window while building it. makes for smoother 'paint'
-    #     except Exception as e:
-    #         print('*** Exception setting alpha channel to zero while creating window ***', e)
+        if not window.NoTitleBar:
+            root.withdraw()
+        else:
+            try:
+                if not running_mac() or \
+                        (running_mac() and not window.NoTitleBar) or \
+                        (running_mac() and window.NoTitleBar and not _mac_should_apply_notitlebar_patch()):
+                    root.attributes('-alpha', 0)  # hide window while building it. makes for smoother 'paint'
+            except Exception as e:
+                print('*** Exception setting alpha channel to zero while creating window ***', e)
 
     if window.BackgroundColor is not None and window.BackgroundColor != COLOR_SYSTEM_DEFAULT:
         root.configure(background=window.BackgroundColor)
@@ -18406,22 +18409,26 @@ def StartupTK(window):
         root.bind("<Control-Down>", window._move_callback)
 
     window.set_icon(window.WindowIcon)
-    try:
-        alpha_channel = 1 if window.AlphaChannel is None else window.AlphaChannel
-        if root.state() == 'withdrawn':
-            if alpha_channel != 1:
-                root.attributes('-alpha', alpha_channel)  # wrapper already exists here, so this sticks on X11
-            root.deiconify()  # first and only map
-        else:
+    alpha_channel = 1 if window.AlphaChannel is None else window.AlphaChannel
+    if window.NoTitleBar:
+        try:
+            alpha_channel = 1 if window.AlphaChannel is None else window.AlphaChannel
             root.attributes('-alpha', alpha_channel)  # Make window visible again
-    except Exception as e:
-        print('**** Error making window visible after it was created ****', e)
-    # try:
-    #     alpha_channel = 1 if window.AlphaChannel is None else window.AlphaChannel
-    #     root.attributes('-alpha', alpha_channel)  # Make window visible again
-    # except Exception as e:
-    #     print('**** Error setting Alpha Channel to {} after window was created ****'.format(alpha_channel), e)
-    #     # pass
+        except Exception as e:
+            print('**** Error setting Alpha Channel to {} after window was created ****'.format(alpha_channel), e)
+    else:
+        try:
+            if root.state() == 'withdrawn':
+                if alpha_channel != 1:
+                    root.attributes('-alpha', alpha_channel)  # wrapper already exists here, so this sticks on X11
+                root.deiconify()  # first and only map
+            else:
+                root.attributes('-alpha', alpha_channel)  # Make window visible again
+        except Exception as e:
+            print('**** Error making window visible after it was created ****', e)
+
+
+
 
     if window.ReturnKeyboardEvents and not window.NonBlocking:
         root.bind("<KeyRelease>", window._KeyboardCallback)
